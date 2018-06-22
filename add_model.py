98bc7ebccbf002a9a34acfe49c5291c8c2c99841
Myse1f <602149025@qq.com>
2018-06-22 12:07:40 +0800

    add add_model

---
+++ add_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recite.settings")
+
+
+import django
+if django.VERSION >= (1, 7):  # 自动判断版本
+    django.setup()
+
+from HelloWord.models import Vocabulary
+from HelloWord.models import Word
+
+CET4 = Vocabulary.objects.create(name='四级')
+CET6 = Vocabulary.objects.create(name='六级')
+POST = Vocabulary.objects.create(name='考研')
+IELTS = Vocabulary.objects.create(name='雅思')
+TOEFL = Vocabulary.objects.create(name='托福')
+data = {
+    'IELTS.txt': IELTS,
+    'TOEFL.txt': TOEFL,
+    'CET4.txt': CET4,
+    'CET6.txt': CET6,
+    'POST.txt': POST
+
+}
+
+
+def create(filename, vocabulary):
+    with open(filename) as f:
+        for line in f:
+            name, explanation =\
+                line.split('|')[0], line.split('|')[1].strip('\n')
+            example = 'This an example for {}'.format(name) # pretend it's an example
+            word = Word.objects.create(name=name, explanation=explanation, example=example)
+            vocabulary.words.add(word)
+    print('{}导入完成'.format(filename))
+
+
+def main():
+    for x, y in data.items():
+        create(x, y)
+
+
+if __name__ == "__main__":
+    main()
+    print('Done!')
